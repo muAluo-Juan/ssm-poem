a823ce43d217ce1c61936cf00a9d8befad200f15
jzw <jzw@window10>
2020-04-04 20:44:37 +0800

    返回状态

---
+++ src/main/java/po/Result.java
@@ -0,0 +1,16 @@
+package po;
+
+public class Result {
+	private int code;
+	public Result(int i) {
+		// TODO Auto-generated constructor stub
+		code=i;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+}
