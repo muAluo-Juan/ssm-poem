8cbae071d8262b8ac5144573e918a3c27b3c8a40
Administrator <Administrator@DESKTOP-JUAN>
2020-04-05 15:28:07 +0800

    测试vue+springmvc

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,11 +1,17 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import po.NormalUser;
 
 @Controller
 @RequestMapping("/index")
 public class IndexController {
+	@CrossOrigin(origins = "*", maxAge = 3600)
 	@RequestMapping("/login")
 	public String handleRequest() {
 		return "login";  //返回login.jsp
@@ -21,5 +27,15 @@ public class IndexController {
 	@RequestMapping("/user/community")
 	public String entercommunity() {
 		return "/normalUser/community";
+	}
+	
+	@CrossOrigin(origins = "*", maxAge = 3600)
+	@RequestMapping(value="/test",method= RequestMethod.GET)
+	public @ResponseBody
+	NormalUser what(){
+		System.out.print("好开心啊有人访问我！");
+		NormalUser no = new NormalUser();
+		no.setPenName("我靠");
+		return no;
 	}
 }
